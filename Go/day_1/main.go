944978aff4d3ca4e9bda09b987dc1718e1491179
Ernesto <davidedpg10@gmail.com>
2023-12-07 07:07:55 +0000

    Advent of Code: Day 1: Part 2

--- Go/day_1/main.go
+++ Go/day_1/main.go
@@ -3,44 +3,64 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
+var StringIntMap = map[string]int{
+	"zero":  0,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
 func main() {
-	// Calibration document begin
+	partOne()
+	partTwo()
+}
+
+func partOne() {
 	fileImport, err := os.ReadFile("input.txt")
-	calibrationDocument := string(fileImport)
-	//fmt.Println(calibrationDocument)
 	if err != nil {
 		panic(fmt.Errorf("Error reading file: %w", err))
 	}
+	calibrationDocument := string(fileImport)
 
-	// Calibration document end
 	var calibrationValues []int
-	var sum int = 0
 	for _, v := range strings.Split(calibrationDocument, "\n") {
-		fmt.Println("Line: ", v)
-		intArray := GetInts(v)
-		fmt.Println("Ints returned: ", intArray)
-		if len(intArray) > 0 {
-			v1 := intArray[0]
-			v2 := intArray[len(intArray)-1]
-			computed, err := strconv.Atoi(fmt.Sprintf("%d%d", v1, v2))
-			if err != nil {
-				panic(err)
-			}
-			calibrationValues = append(calibrationValues, computed)
+		intSlice := ParseNumbersFromString(v)
+		if i := FirstLastIntsFromSlice(intSlice); i != -1 {
+			calibrationValues = append(calibrationValues, i)
 		}
 	}
-	fmt.Printf("%#v\n", calibrationValues)
-	for _, v := range calibrationValues {
-		sum = sum + v
-	}
-	fmt.Println("Total value: ", sum)
+	fmt.Println("Part 1: Total value: ", SumIntSlice(calibrationValues))
 }
 
-func GetInts(str string) []int {
+func partTwo() {
+	fileImport, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(fmt.Errorf("Error reading file: %w", err))
+	}
+	calibrationDocument := string(fileImport)
+
+	var calibrationValues []int
+	for _, v := range strings.Split(calibrationDocument, "\n") {
+		intSlice := ParseNumberFromStringByWords(v)
+		if i := FirstLastIntsFromSlice(intSlice); i != -1 {
+			calibrationValues = append(calibrationValues, i)
+		}
+	}
+	fmt.Println("Part 2: Total value: ", SumIntSlice(calibrationValues))
+}
+
+func ParseNumbersFromString(str string) []int {
 	var ints []int
 	for _, v := range str {
 		if i, err := strconv.Atoi(string(v)); err == nil {
@@ -48,4 +68,69 @@ func GetInts(str string) []int {
 		}
 	}
 	return ints
+}
+
+func ParseNumberFromStringByWords(str string) []int {
+	// Map will be populated as follows:
+	// Field 1: Index where number was found
+	// Field 2: Number found as word
+	numMap := make(map[int]int)
+	var ints []int
+
+	// identifying spelled out numbers and adding to numMap
+	// the last index variable keeps track of the last index found
+	// additionally when a value is found we add 1 to the last index so it doesn't start in the same
+	// spot as the last index attempt, which results in a loop
+	for numberName, num := range StringIntMap {
+		lastIndex := 0
+		for lastIndex >= 0 {
+			idx := strings.Index(strings.ToLower(str[lastIndex:]), strings.ToLower(numberName))
+			if idx != -1 {
+				numMap[idx+lastIndex] = num
+				lastIndex = idx + lastIndex + 1
+			} else {
+				lastIndex = idx
+			}
+		}
+	}
+	// Identifying digit numbers
+	for idx, v := range str {
+		if i, err := strconv.Atoi(string(v)); err == nil {
+			numMap[idx] = i
+		}
+	}
+
+	// Separating and sorting the indexes
+	var indexes []int
+	for i := range numMap {
+		indexes = append(indexes, i)
+	}
+	sort.Ints(indexes)
+
+	// Aggregating for return
+	for _, v := range indexes {
+		ints = append(ints, numMap[v])
+	}
+	return ints
+}
+
+func FirstLastIntsFromSlice(intSlice []int) int {
+	if len(intSlice) > 0 {
+		v1 := intSlice[0]
+		v2 := intSlice[len(intSlice)-1]
+		computed, err := strconv.Atoi(fmt.Sprintf("%d%d", v1, v2))
+		if err != nil {
+			panic(err)
+		}
+		return computed
+	}
+	return -1
+}
+
+func SumIntSlice(intSlice []int) int {
+	sum := 0
+	for _, v := range intSlice {
+		sum = sum + v
+	}
+	return sum
 }
